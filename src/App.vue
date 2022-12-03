9b8647973725df2618bf847cd05eba3bfc6d307c
Linus Meier <linusmeier93@ingesa.ch>
2022-12-03 23:10:46 +0100

    first things

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,59 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <v-container >
+        <v-responsive class="d-flex align-center text-center fill-height">
+          <v-autocomplete
+            rounded
+            v-bind="this.selectedCiv"
+            label="Search civ"
+            :items="civs"
+            item-title="name"
+            item-value="name"
+          ></v-autocomplete>
+            <v-row align="center" justify="center" no-gutters cols="12" >
+              <div v-for="civ in this.civs" :key="civ.name">
+                <CivCard :civ="civ.name" :icon="civ.icon" />
+            </div>
+              </v-row>
+        </v-responsive>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
-<script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+
+
+<script>
+import CivCard from "@/components/CivCard.vue";
+
+export default {
+  components: {
+    CivCard,
+  },
+
+  data() {
+    return {
+      selectedCiv: "",
+      civs: [
+        { name: "Franks", icon: "FranksIcon" },
+        { name: "Mayans", icon: "MayansIcon" },
+        { name: "Berbers", icon: "BerbersIcon" },
+        { name: "Saracens", icon: "SaracensIcon" },
+        { name: "Hindustanis", icon: "HindustanisIcon" },
+        { name: "Koreans", icon: "KoreansIcon" },
+        { name: "Britons", icon: "BritonsIcon" },
+        { name: "Magyars", icon: "MagyarsIcon" },
+        { name: "Teutons", icon: "TeutonsIcon" },
+        { name: "Celts", icon: "CeltsIcon" },
+      ],
+    };
+  },
+
+  // methods: {
+  //   getImage(imagePath) {
+  //     return require(imagePath);
+  //   },
+  // }
+};
 </script>
---
+++ src/components/CivCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-card rounded="pill" :prepend-icon="icon" class="ma-2 pa-2">
+    <template v-slot:title> {{ civ }} </template>
+    </v-card>
+</template>
+  
+<script >
+export default {
+  props: {
+    civ: String,
+    icon: String
+  },
+
+  mounted() {
+    console.log(this.civ)
+  }
+}
+</script>
+  
